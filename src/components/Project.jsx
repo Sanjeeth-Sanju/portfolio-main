102c7dbca3c1675e7b86ad3a2670ea5cbf8c3ac0
Sanjeeth Balasubramani <sanjeeth022503@gmail.com>
2026-02-16 11:55:27 +0530

    gitHub links updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,19 +9,19 @@ function Project(){
     <ProjectCard 
     name="empowHER - Women Empowerment Platform" 
     description="Built a responsive React based platform during a 72 hour hackathon focused on women’s health, career support, financial literacy, and community resources. Designed modular component architecture with scalable structure and mobile first responsiveness."
-     gitLink = "https://lnkd.in/gBBqJH4F" liveLink = "https://lnkd.in/gviFv_7D" 
+     gitLink = "https://github.com/Sanjeeth-Sanju/empowHER.git" liveLink = "https://empowher-w7y6.onrender.com" 
     />
 
     <ProjectCard 
     name="Cinemaah-Scribbles - Full Stack Movie Diary" 
     description="Developed a full-stack web application with admin-only CRUD operations, TMDb API integration, and PostgreSQL database. Implemented server-side rendering using EJS and secured environment variables using .env configuration."
-     gitLink = "https://lnkd.in/gGdYRnCR" liveLink = "https://lnkd.in/gSZVbHXy" 
+     gitLink = "https://github.com/Sanjeeth-Sanju/cinemaah-scribbles.git" liveLink = "https://cinemaahscribbles.live/" 
     /> 
 
     <ProjectCard 
-    name="Weather Forecast App" 
+    name="WeatheryCast - Weather Forecast Web Application" 
     description="Built a real-time weather application using Node.js, Express, and EJS with OpenWeather API integration. Implemented backend API handling, dynamic rendering, and error management for unsupported locations."
-     gitLink = "https://lnkd.in/gAu_hKuv" liveLink = "https://lnkd.in/ggnwkeEW" 
+     gitLink = "https://github.com/Sanjeeth-Sanju/WeatheryCast.git" liveLink = "https://weatherycast.onrender.com" 
     />       
   </div>
 </section>
